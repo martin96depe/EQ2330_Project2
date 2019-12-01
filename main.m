0dba8c67e99cc0ec56d35aaff1e101fa4a8e98cb
Martin De Pellegrini <55257805+martin96depe@users.noreply.github.com>
2019-12-01 13:05:14 +0100

    01/12

--- main.m
+++ main.m
@@ -4,16 +4,8 @@
 %image code using a DCT for 8x8 blocks of an image
 %% Blockwise 8x8 DCT
 M = 8;
-dct_mask = zeros(M);
+dct_mask = mydct_mask(M);
 
-for x=0:M-1
-    for y=0:M-1
-        if x==0
-            dct_mask(x+1,y+1) = sqrt(1/M);
-        else
-            dct_mask(x+1,y+1) = sqrt(2/M)*cos(((2*y +1)*x*pi)/(2*M));
-        end
-    end
-end
+
 
 
